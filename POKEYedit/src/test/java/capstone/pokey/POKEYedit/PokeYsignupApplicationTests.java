9afcee5888e775ad7edc0c63cb175a6c8dc1cd0e
SeungTaeGit <129585999+SeungTaeGit@users.noreply.github.com>
2023-06-03 23:17:44 +0900

    Add files via upload

---
+++ POKEYedit/src/test/java/capstone/pokey/POKEYedit/PokeYsignupApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeYsignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
